6d0df976aac5b3b0ff7ca0c0a5b4e5ee6b54ee0c
ATAUR RAHMAN ASHIS <148003286+Ataur6609@users.noreply.github.com>
2023-11-26 06:39:46 +0600

    Add files via upload

---
+++ Multiline_statement.py
@@ -0,0 +1,6 @@
+name= 'Ataur Rahman Ashis'
+University='Bangabandhu Sheikh Mujibur Rahman Digital University'
+Age='22'
+print("My name is:", name)
+print("My age is:", Age)
+print("My University name is:", University)
